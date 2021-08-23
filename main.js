6bcb4163ad9de192287de06a2aee6a1b78acbafe
Rishabh Varshney <right.rishabh@gmail.com>
2021-08-23 20:14:40 +0530

    added animations

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
+const nav = document.querySelector(".navbar");
+
 $(document).ready(function () {
     $(window).scroll(function () {
       let scroll = $(window).scrollTop();
-      if (scroll > 10) {
+      if (scroll > 50) {
+        nav.style.height = "50px";
         $(".navbar").css("background", "white");
         $("a").css("color", "black");
+        $(".logo").css("color", "black");
       } else {
+        nav.style.height = "70px";
         $(".navbar").css("background", "transparent");
         $("a").css("color", "white");
+        $(".logo").css("color", "white");
       }
     });
   });
